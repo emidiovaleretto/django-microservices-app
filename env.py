1d340a7e93a1959b5952e691aa2b655d5ad450b8
Emidio Valeretto <emidio.valereto@gmail.com>
2024-03-01 19:46:39 +0000

    remove env.py from git

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "django-insecure-wdl4w0w1hk(8ufoeyv_=s%5uz#()nb90!yjod4wv(!*@@y73cl")
-os.environ.setdefault("ALLOWED_LIST", "8000-emidiovaler-djangomicro-ez4ajwiarzy.ws-eu108.gitpod.io")
