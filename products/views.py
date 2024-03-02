a14c131cca599c250f775e6b91726182fd88dfd4
Emidio Valeretto <emidio.valereto@gmail.com>
2024-03-02 00:33:49 +0000

    chore: implement ProductViewSet and assign an URL path to the api routes

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk):
+        pass
+
+    def update(self, request, pk):
+        pass
+
+    def destroy(self, request, pk):
+        pass
