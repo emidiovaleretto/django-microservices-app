dbc3c87e17abe683b9845527634e277674b68921
Emidio Valeretto <emidio.valereto@gmail.com>
2024-03-02 00:57:22 +0000

    chore: implement both retrieve and destroy methods

--- products/views.py
+++ products/views.py
@@ -5,24 +5,18 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-class ProductViewSet(viewsets.ViewSet):
-    def list(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            return super().retrieve(request, *args, **kwargs)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def create(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def retrieve(self, request, pk):
-        pass
-
-    def update(self, request, pk):
-        pass
-
-    def destroy(self, request, pk):
-        pass
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
